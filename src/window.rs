64dd03f894b6a200a8aa8e26f7641cc4b4e9cbcf
Gabriel Cardoso <gabrielhsc95@gmail.com>
2025-05-17 23:38:42 -0500

    drag and maximize

--- src/window.rs
+++ src/window.rs
@@ -1,17 +1,22 @@
+use leptos::ev::MouseEvent;
+use leptos::html::Div;
 use leptos::prelude::*;
+use log::debug;
 pub struct WindowState<Content: IntoView + 'static> {
     title: String,
     content: Content,
-    x: i32,
+    x: RwSignal<i32>,
     default_x: i32,
-    y: i32,
+    y: RwSignal<i32>,
     default_y: i32,
     width: i32,
     default_width: i32,
     height: i32,
     default_height: i32,
-    is_opened: RwSignal<bool>,
     is_minimized: RwSignal<bool>,
+    is_maximized: RwSignal<bool>,
+    is_opened: RwSignal<bool>,
+    drag: RwSignal<Option<(i32, i32)>>,
 }
 
 impl<Content: IntoView + 'static> WindowState<Content> {
@@ -19,24 +24,31 @@ impl<Content: IntoView + 'static> WindowState<Content> {
         Self {
             title,
             content,
-            x,
+            x: RwSignal::new(x),
             default_x: x,
-            y,
+            y: RwSignal::new(y),
             default_y: y,
             width,
             default_width: width,
             height,
             default_height: height,
-            is_opened: RwSignal::new(true),
             is_minimized: RwSignal::new(false),
+            is_maximized: RwSignal::new(false),
+            is_opened: RwSignal::new(true),
+            drag: RwSignal::new(None),
         }
     }
 }
 
 #[component]
 pub fn WindowWidget<Content: IntoView + 'static>(state: WindowState<Content>) -> impl IntoView {
-    let is_opened_toggle = move |_| {
-        state.is_opened.update(|open: &mut bool| *open = !*open);
+    let div_ref: NodeRef<Div> = NodeRef::new();
+    let body_style = RwSignal::new(String::from(""));
+
+    let is_maximized_toggle = move |_| {
+        state
+            .is_maximized
+            .update(|maximize: &mut bool| *maximize = !*maximize);
     };
 
     let is_minimized_toggle = move |_| {
@@ -45,16 +57,94 @@ pub fn WindowWidget<Content: IntoView + 'static>(state: WindowState<Content>) ->
             .update(|minimize: &mut bool| *minimize = !*minimize);
     };
 
+    let is_opened_toggle = move |_| {
+        state.is_opened.update(|open: &mut bool| *open = !*open);
+    };
+
+    let start_drag = move |mouse_event: MouseEvent| {
+        state
+            .drag
+            .update(|drag| *drag = Some((mouse_event.client_x(), mouse_event.client_y())));
+        body_style.set("user-select: none;".to_string());
+    };
+
+    let drag_window = move |mouse_event: MouseEvent| {
+        if state.drag.read().is_some() {
+            let (initial_x, initial_y) = state.drag.read().unwrap();
+            let mouse_x = mouse_event.client_x();
+            let mouse_y = mouse_event.client_y();
+            let delta_x = mouse_x - initial_x;
+            let delta_y = mouse_y - initial_y;
+            state
+                .drag
+                .update(|drag| *drag = Some((mouse_event.client_x(), mouse_event.client_y())));
+            state.x.update(|x| *x += delta_x);
+            state.y.update(|y| *y += delta_y);
+        }
+    };
+
+    let stop_drag = move |_| {
+        state.drag.update(|drag| *drag = None);
+        body_style.set("".to_string());
+    };
+
     view! {
         <div
             class="window"
-            style:width=move || format!("{}px", state.width)
-            style:height=move || {if *state.is_minimized.read() {"37px".to_string()} else {format!("{}px", state.height)}}
-            style:display=move || { if *state.is_opened.read() {"block"} else {"none"}}
-            style:top=move || format!("{}px", state.x)
-            style:left=move || format!("{}px", state.y)
+            node_ref=div_ref
+            style:width=move || {
+                if *state.is_maximized.read() {
+                    "98%".to_string()
+                } else {
+                    format!("{}px", state.width)
+                }
+            }
+            style:height=move || {
+                if *state.is_minimized.read() {
+                    "37px".to_string()
+                }
+                else if *state.is_maximized.read(){
+                    "87%".to_string()
+                }
+                else {
+                    format!("{}px", state.height)
+                }
+            }
+            style:display=move || {
+                if *state.is_opened.read() {
+                    "block"
+                } else {
+                    "none"
+                }
+            }
+            style:top=move || {
+                if *state.is_maximized.read() {
+                    "110px".to_string()
+                } else {
+                    format!("{}px", state.y.read())
+                }
+            }
+            style:left=move || {
+                if *state.is_maximized.read() {
+                    "10px".to_string()
+                } else {
+                    format!("{}px", state.x.read())
+                }
+            }
+            style:z-index=move || {
+                if state.drag.read().is_some() || *state.is_maximized.read() {
+                    "99"
+                } else {
+                    "0"
+                }
+            }
         >
-            <div class="window-title">
+            <div
+                class="window-title"
+                on:mousedown=start_drag
+                on:mousemove=drag_window
+                on:mouseup=stop_drag
+            >
                 <h1>{state.title}</h1>
                 <div class="window-title-buttons">
                     <div
@@ -63,7 +153,12 @@ pub fn WindowWidget<Content: IntoView + 'static>(state: WindowState<Content>) ->
                     >
                         "−"
                     </div>
-                    <div class="window-title-button maximize">"□"</div>
+                    <div
+                        class="window-title-button maximize"
+                        on:click=is_maximized_toggle
+                    >
+                        "□"
+                    </div>
                     <div
                         class="window-title-button close"
                         on:click=is_opened_toggle
@@ -74,5 +169,10 @@ pub fn WindowWidget<Content: IntoView + 'static>(state: WindowState<Content>) ->
             </div>
             <div class="window-content">{state.content}</div>
         </div>
+        <style>
+            {"body {"}
+                {move || body_style.get()}
+            {"}"}
+        </style>
     }
 }
