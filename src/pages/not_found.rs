170ebdb0b568c9db53e8e48880beb75b15613c77
Gabriel Cardoso <gabrielhsc95@gmail.com>
2025-12-21 21:50:56 -0600

    fix 404

--- src/pages/not_found.rs
+++ src/pages/not_found.rs
@@ -1,33 +1,31 @@
 use yew::prelude::*;
+use rand::prelude::*;
 
 #[function_component(PageNotFound)]
 pub fn page_not_found() -> Html {
-    // let mut rng = rand::rng();
-    // let animals = vec![
-    //     "monkey", "platypus", "phoenix", "koala", "house", "cat", "dog", "panda", "axolotl",
-    //     "dolphin",
-    // ];
-    // let animal = animals
-    //     .choose(&mut rng)
-    //     .expect("There should have been an animal option to choose from!");
-    // let adjectives = vec![
-    //     "curious",
-    //     "crazy",
-    //     "majestic",
-    //     "ancient",
-    //     "gentle",
-    //     "brilliant",
-    //     "calm",
-    //     "humble",
-    //     "tenacious",
-    //     "loyal",
-    // ];
-    // let adjective = adjectives
-    //     .choose(&mut rng)
-    //     .expect("There should have been an adjective option to choose from!");
-    let animal = "monkey";
-    let adjective = "curious";
-
+    let mut rng = rand::thread_rng();
+    let animals = vec![
+        "monkey", "platypus", "phoenix", "koala", "house", "cat", "dog", "panda", "axolotl",
+        "dolphin",
+    ];
+    let animal = animals
+        .choose(&mut rng)
+        .expect("There should have been an animal option to choose from!");
+    let adjectives = vec![
+        "curious",
+        "crazy",
+        "majestic",
+        "ancient",
+        "gentle",
+        "brilliant",
+        "calm",
+        "humble",
+        "tenacious",
+        "loyal",
+    ];
+    let adjective = adjectives
+        .choose(&mut rng)
+        .expect("There should have been an adjective option to choose from!");
     html! {
         <div>
             <p><b>{"Page not Found"}</b></p>
